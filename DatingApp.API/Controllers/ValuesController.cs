22c630319944526d55ad59d393917b2b3a2d5db1
punam.edominer@gmail.com <punam.edominer@gmail.com>
2020-01-18 23:59:30 +0530

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly DataContext _dataContext;
+        public ValuesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        //[HttpGet]
+        //public IActionResult Get()
+        //{
+        //    var values = _dataContext.Values.ToList();
+
+        //    return Ok(values);
+        //}
+        //GET: api/Values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _dataContext.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+
+        // GET: api/Values/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _dataContext.Values.FindAsync(id);
+            return Ok(value);
+        }
+
+        // POST: api/Values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
